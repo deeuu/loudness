945da0a66f6a7ad10e2bc5f2e1a3b7722f458bc5
deeuu <contactdominicward@gmail.com>
2015-05-05 21:30:12 +0100

    Commented on PeakFollower

--- src/modules/PeakFollower.h
+++ src/modules/PeakFollower.h
@@ -27,31 +27,24 @@ namespace loudness{
     /**
      * @class PeakFollower
      *
-     * @brief Implements a first-order low-pass filter with seperate attack and
-     * release time-constants. This module will filter all signals in the input
-     * SignalBank independently and therefore gives an output SignalBank of the
-     * same shape.
+     * @brief Implements a peak follower by outputting the absolute value of the input
+     * sample if it is greater than the previous output sample. If this
+     * evaluates to false, an exponential decay with time-constant defined by
+     * releaseTime is applied to the peak. This essentially involves
+     * subtracting a proportion of the previous output sample from itself.  
+     * This module will filter all signals in the input SignalBank independently
+     * and therefore gives an output SignalBank of the same shape.
      *
      * The time-step used in the calculation of filter coefficients is derived
      * from the input SignalBank's frame rate, which by default is equal to the
      * sampling frequency.
-     *
-     * REFERENCES:
-     *
-     * Zolzer, U. (2002). DAFX - Digital Audio Effects. (U. Zolzer, Ed.) 
-     * John Wiley & Sons, Ltd.
-     *
-     * Glasberg, B. R., & Moore, B. C. J. (2002). A Model of Loudness Applicable
-     * to Time-Varying Sounds. Journal of the Audio Engineering Society, 50(5),
-     * 331–342.
-     *
      */
     class PeakFollower : public Module
     {
     public:
  
-        /** Constructs an PeakFollower with attackTime and releaseTime in
-         * seconds */
+        /** Constructs an PeakFollower an exponential decay defined by the
+         * time-constant releaseTime in seconds */
         PeakFollower(Real releaseTime);
 
         virtual ~PeakFollower();
