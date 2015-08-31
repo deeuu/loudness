355a30b2ce9424a19352c816371cb35ecff9f359
deeuu <contactdominicward@gmail.com>
2015-08-31 12:35:59 +0100

    Fix and cleaned test

--- python/tests/test_AudioFileProcessor.py
+++ python/tests/test_AudioFileProcessor.py
@@ -3,7 +3,8 @@ import loudness as ln
 
 model = ln.DynamicLoudnessGM2002()
 model.setRate(250)
-model.setOutputModulesToAggregate(['LongTermLoudness'])
+outputs = ['ShortTermLoudness', 'LongTermLoudness']
+model.setOutputsToAggregate(outputs)
 
 processor = ln.AudioFileProcessor('../../wavs/pureTones/pureTone_1000Hz_40dBSPL_32000Hz.wav')
 processor.setGainInDecibels(10)
@@ -11,8 +12,8 @@ processor.initialize(model)
 
 processor.processAllFrames(model)
 
-bankSTL = model.getOutputModuleSignalBank('LongTermLoudness')
-aggregatedFrames = bankSTL.getAggregatedSignals()
-
-plt.plot(aggregatedFrames.flatten())
+for output in outputs:
+    bankSTL = model.getOutput(output)
+    aggregatedFrames = bankSTL.getAggregatedSignals()
+    plt.plot(aggregatedFrames.flatten())
 plt.show()
